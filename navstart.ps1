71cebbfff48a0648aef98cebda58dca5eee68db3
Markus Lippert <lippertmarkus@gmx.de>
2021-02-11 09:54:51 +0100

    Update navstart.ps1

--- navstart.ps1
+++ navstart.ps1
@@ -1,4 +1,8 @@
-. "C:\run\my\CC-DownloadCustomScripts.ps1"
+$downloadCustomScriptsScript = "C:\run\my\CC-DownloadCustomScripts.ps1"
+
+if (Test-Path $downloadCustomScriptsScript) {
+  . $downloadCustomScriptsScript
+}
 
 # invoke default
 . (Join-Path $runPath $MyInvocation.MyCommand.Name)
